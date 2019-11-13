942f28ba918c74853f22c0115af36c785550f455
Radhika Modi <rhmodi@enacton.com>
2019-11-13 19:23:12 +0530

    Added Category filter and a reset button

--- resources/js/views/products/index.vue
+++ resources/js/views/products/index.vue
@@ -2,7 +2,15 @@
   <div class="app-container">
     <h1>My Products</h1>
     <div class="filter-container">
-      <el-input v-model="search" placeholder="name" style="width: 200px;" class="filter-item" />
+      <el-input v-model="searchName" placeholder="name" style="width: 200px;" class="filter-item" />
+      <el-select
+        v-model="searchCategory"
+        placeholder="please select category"
+        style="width: 200px; top: -3.7px;"
+      >
+        <el-option v-for="item in category" :key="item.id" :label="item.name" :value="item.id"></el-option>
+      </el-select>
+      <el-button class="filter-item" icon="el-icon-refresh" @click="searchName=''; searchCategory='';">Reset</el-button>
       <el-button class="filter-item" type="primary" icon="el-icon-search">Search</el-button>
       <el-button
         :loading="downloadLoadingLess"
@@ -20,12 +28,7 @@
       :page.sync="currentPage"
       @pagination="loadNewPage"
     ></Pagination>
-    <el-table
-      v-loading="loading"
-      :data="products.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
-      stripe
-      style="width: 100%"
-    >
+    <el-table v-loading="loading" :data="filterProductsTable(products)" stripe style="width: 100%">
       <el-table-column prop="name" label="Product Name"></el-table-column>
       <el-table-column prop="brand" label="Brand"></el-table-column>
       <!-- <el-table-column prop="description" labe l="Description" width="200px"></el-table-column> -->
@@ -77,7 +80,8 @@ export default {
       editProduct: false,
       currentProductInfo: null,
       downloadLoadingLess: false,
-      search: '',
+      searchName: '',
+      searchCategory: '',
     };
   },
   created() {
@@ -103,6 +107,27 @@ export default {
     async getCategories() {
       this.category = await categoryResource.list({});
       console.log(this.category);
+    },
+    filterProductsTable(products) {
+      let filteredProducts = [];
+      if (this.searchName && this.searchCategory) {
+        filteredProducts = this.products
+          .filter(data =>
+            data.name.toLowerCase().includes(this.searchName.toLowerCase())
+          )
+          .filter(data => data.category_id === this.searchCategory);
+      } else if (this.searchName) {
+        filteredProducts = this.products.filter(data =>
+          data.name.toLowerCase().includes(this.searchName.toLowerCase())
+        );
+      } else if (this.searchCategory) {
+        filteredProducts = this.products.filter(
+          data => data.category_id === this.searchCategory
+        );
+      } else {
+        filteredProducts = products;
+      }
+      return filteredProducts;
     },
     getCategoryName(id) {
       return this.category[id - 1].name;
